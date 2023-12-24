7d86bd1bf260d7c419a1b8577ae5783eac1d50eb
ny-plus-ny <ny.plus.ny@gmail.com>
2023-12-24 17:44:31 +0900

    next-auth 설정 및 googleProvider 추가

---
+++ src/app/_component/SessionProvider.tsx
@@ -0,0 +1,19 @@
+"use client"
+
+import { SessionProvider } from "next-auth/react"
+export default SessionProvider;
+
+// "use client";
+ 
+// import { SessionProvider } from "next-auth/react";
+// import { ReactNode } from "react";
+// import { Session } from "next-auth";
+ 
+// type Props = {
+//     session?: Session | null;
+//     children: ReactNode;
+// };
+ 
+// export default function AuthSession({ session, children }: Props) {
+//     return <SessionProvider session={session}>{children}</SessionProvider>;
+// }
