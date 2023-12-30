4e8d33d5542496527b146a51e0614328a870475c
ny-plus-ny <ny.plus.ny@gmail.com>
2023-12-30 20:55:05 +0900

    memolist 불필요한 리렌더링 방지를 위해 useEffect 추가

--- src/app/(afterLogin)/memoList/page.tsx
+++ src/app/(afterLogin)/memoList/page.tsx
@@ -1,38 +1,37 @@
-"use client"
+'use client';
 
-import FirebaseConfig from "@/app/_component/firebaseConfig/firebaseConfig";
-import { ref, onValue, get } from "firebase/database";
-import { useEffect, useState } from "react";
-import styled from "styled-components";
+import FirebaseConfig from '@/app/_component/firebaseConfig/firebaseConfig';
+import { ref, get } from 'firebase/database';
+import { useEffect, useState } from 'react';
+import styled from 'styled-components';
 
-export default  function MemoList(){
-    const database = FirebaseConfig();
-    const memoListRef = ref(database, "Customer/" + "test" + "/contents");
+export default function MemoList() {
+  const database = FirebaseConfig();
+  const memoListRef = ref(database, 'Customer/' + 'test' + '/contents');
 
-    const [isData, setIsData] = useState("");
-    get(memoListRef).then((snapshot)=>{
-        if(snapshot.exists()){
-            const data = snapshot.val();
-            setIsData(data)
-        }
-        // console.log("메모리스트", data)
+  const [isData, setIsData] = useState('');
 
-    })
+  useEffect(() => {
+    get(memoListRef).then(snapshot => {
+      if (snapshot.exists()) {
+        const data = snapshot.val();
+        setIsData(data);
+      }
+    });
+  }, []);
 
-
-    return(
-        <Container>
-                <div>내용:::::     {isData}</div>
-
-        </Container>
-    )
+  return (
+    <Container>
+      <div>내용 {isData}</div>
+    </Container>
+  );
 }
 
 const Container = styled.div`
-    width: 100dvw;
-    height: 100dvh;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: bisque;
-`
+  width: 100dvw;
+  height: 100dvh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: bisque;
+`;
